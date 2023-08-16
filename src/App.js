1b4a864a619a14b72b853d79f6ccfdde1319863e
Ivan Afanasyeu <--local>
2023-08-16 13:42:12 +0200

    base files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer.js'
+import Header from './components/Header.js'
+import Main from './components/Main.js'
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>testing</header>
-		</div>
-	);
+class App extends React.Component {
+  render() { 
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
